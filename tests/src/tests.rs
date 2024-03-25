b9966e0d811489fde06bf7fc2b3b63e134c7c30b
Dylan Duan <duanyytop@gmail.com>
2024-03-25 14:18:46 +0800

    tests: add unique type tests

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -1,73 +1,121 @@
-use super::*;
-use ckb_testtool::context::Context;
+use crate::{assert_script_error, Loader};
+use blake2b_rs::Blake2bBuilder;
 use ckb_testtool::ckb_types::{
     bytes::Bytes,
-    core::TransactionBuilder,
+    core::{TransactionBuilder, TransactionView},
     packed::*,
     prelude::*,
 };
-use ckb_testtool::ckb_error::Error;
+use ckb_testtool::{builtin::ALWAYS_SUCCESS, context::Context};
 
 const MAX_CYCLES: u64 = 10_000_000;
 
 // error numbers
-const ERROR_EMPTY_ARGS: i8 = 5;
+const UNIQUE_TYPE_ID_INVALID: i8 = 5;
+const INPUT_UNIQUE_CELL_MISMATCH: i8 = 6;
 
-fn assert_script_error(err: Error, err_code: i8) {
-    let error_string = err.to_string();
-    assert!(
-        error_string.contains(format!("error code {} ", err_code).as_str()),
-        "error_string: {}, expected_error_code: {}",
-        error_string,
-        err_code
-    );
+#[derive(PartialEq)]
+enum UniqueError {
+    NoError,
+    UniqueTypeIdInvalid,
+    InputUniqueCellMismatch,
 }
 
-#[test]
-fn test_success() {
+fn create_test_context(unique_error: UniqueError) -> (Context, TransactionView) {
     // deploy contract
     let mut context = Context::default();
-    let contract_bin: Bytes = Loader::default().load_binary("unique-cell");
-    let out_point = context.deploy_cell(contract_bin);
+    let unique_bin: Bytes = Loader::default().load_binary("unique-type");
+    let unique_out_point = context.deploy_cell(unique_bin);
+
+    // deploy always_success script
+    let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
 
     // prepare scripts
     let lock_script = context
-        .build_script(&out_point, Bytes::from(vec![42]))
+        .build_script(&always_success_out_point, Default::default())
         .expect("script");
+    let lock_script_dep = CellDep::new_builder()
+        .out_point(always_success_out_point)
+        .build();
 
     // prepare cells
-    let input_out_point = context.create_cell(
+    let empty_input_out_point = context.create_cell(
         CellOutput::new_builder()
-            .capacity(1000u64.pack())
+            .capacity(500u64.pack())
             .lock(lock_script.clone())
             .build(),
         Bytes::new(),
     );
-    let input = CellInput::new_builder()
-        .previous_output(input_out_point)
+    let empty_input = CellInput::new_builder()
+        .previous_output(empty_input_out_point)
         .build();
-    let outputs = vec![
+
+    let mut blake2b = Blake2bBuilder::new(32)
+        .personal(b"ckb-default-hash")
+        .build();
+    blake2b.update(empty_input.as_slice());
+    blake2b.update(&(0u64).to_le_bytes());
+    let mut ret = [0; 32];
+    blake2b.finalize(&mut ret);
+    let unique_type_args = match unique_error {
+        UniqueError::UniqueTypeIdInvalid => Bytes::copy_from_slice(&ret[12..]),
+        _ => Bytes::copy_from_slice(&ret[0..20]),
+    };
+
+    let unique_type_script = context
+        .build_script(&unique_out_point, unique_type_args)
+        .expect("script");
+    let unique_type_script_dep = CellDep::new_builder()
+        .out_point(unique_out_point.clone())
+        .build();
+
+    let unique_input_out_point = context.create_cell(
         CellOutput::new_builder()
             .capacity(500u64.pack())
             .lock(lock_script.clone())
+            .type_(Some(unique_type_script.clone()).pack())
             .build(),
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script)
-            .build(),
-    ];
+        Bytes::new(),
+    );
 
-    let outputs_data = vec![Bytes::new(); 2];
+    let mut inputs = vec![empty_input.clone()];
+    if unique_error == UniqueError::InputUniqueCellMismatch {
+        inputs.push(
+            CellInput::new_builder()
+                .previous_output(unique_input_out_point)
+                .build(),
+        );
+    }
+
+    let outputs = vec![CellOutput::new_builder()
+        .capacity(500u64.pack())
+        .lock(lock_script)
+        .type_(Some(unique_type_script).pack())
+        .build()];
+
+    let outputs_data = vec![Bytes::default()];
+
+    let mut witnesses = vec![];
+    for _ in 0..inputs.len() {
+        witnesses.push(Bytes::from("0x"))
+    }
 
     // build transaction
     let tx = TransactionBuilder::default()
-        .input(input)
+        .inputs(inputs)
         .outputs(outputs)
         .outputs_data(outputs_data.pack())
+        .cell_dep(lock_script_dep)
+        .cell_dep(unique_type_script_dep)
+        .witnesses(witnesses.pack())
         .build();
-    let tx = context.complete_tx(tx);
+    (context, tx)
+}
 
-    // run
+#[test]
+fn test_create_unique_cell_success() {
+    let (mut context, tx) = create_test_context(UniqueError::NoError);
+    let tx = context.complete_tx(tx);
     let cycles = context
         .verify_tx(&tx, MAX_CYCLES)
         .expect("pass verification");
@@ -75,50 +123,17 @@ fn test_success() {
 }
 
 #[test]
-fn test_empty_args() {
-    // deploy contract
-    let mut context = Context::default();
-    let contract_bin: Bytes = Loader::default().load_binary("unique-cell");
-    let out_point = context.deploy_cell(contract_bin);
-
-    // prepare scripts
-    let lock_script = context
-        .build_script(&out_point, Default::default())
-        .expect("script");
-
-    // prepare cells
-    let input_out_point = context.create_cell(
-        CellOutput::new_builder()
-            .capacity(1000u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        Bytes::new(),
-    );
-    let input = CellInput::new_builder()
-        .previous_output(input_out_point)
-        .build();
-    let outputs = vec![
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script)
-            .build(),
-    ];
-
-    let outputs_data = vec![Bytes::new(); 2];
-
-    // build transaction
-    let tx = TransactionBuilder::default()
-        .input(input)
-        .outputs(outputs)
-        .outputs_data(outputs_data.pack())
-        .build();
+fn test_unique_type_id_error() {
+    let (mut context, tx) = create_test_context(UniqueError::UniqueTypeIdInvalid);
     let tx = context.complete_tx(tx);
-
-    // run
     let err = context.verify_tx(&tx, MAX_CYCLES).unwrap_err();
-    assert_script_error(err, ERROR_EMPTY_ARGS);
+    assert_script_error(err, UNIQUE_TYPE_ID_INVALID);
+}
+
+#[test]
+fn test_input_unique_cell_mismatch_error() {
+    let (mut context, tx) = create_test_context(UniqueError::InputUniqueCellMismatch);
+    let tx = context.complete_tx(tx);
+    let err = context.verify_tx(&tx, MAX_CYCLES).unwrap_err();
+    assert_script_error(err, INPUT_UNIQUE_CELL_MISMATCH);
 }
